7ea96fdeacd9ed5d60459b8576a15b2a50fda507
Jgone2 <jgoneit@gmail.com>
2024-07-07 14:13:32 +0900

    feat: setting vite+JS

---
+++ src/components/Main.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Main = async () => {
+  const Main = styled.div`
+    background-color: #282c34;
+    border: 4px solid #61dafb;
+    width: 1200px;
+    height: 500px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    items-align: center;
+  `;
+
+  // const [data, setData] = useState([])
+
+  //   const res = await fetch('https://jsonplaceholder.typicode.com/todos/1',{
+  //     method:'GET'
+  //   })
+  // const fetchData = await res.json()
+  //   console.log(fetchData)
+
+  return (
+    <>
+      <Main>ㅎㅎㅇㅎㅇ</Main>
+    </>
+  );
+};
+
+export default Main;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.modeule.css'
+import WishList from '../components/WishList'
+const Home = () => {
+
+
+  return (
+    <>
+    <WishList />
+        
+    </>
+    
+  )
+}
+
+export default Home
