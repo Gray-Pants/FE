51d8f270690478772fdbf889fef7cc5266fc9206
jun <yoseb95@gmail.com>
2024-07-08 10:21:56 +0900

    feat: login 및 회원가입 페이지 개발 Co-authored-by: jaejun kim <wowns8114@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import SignUp from './pages/Signup'
+import { Routes, Route } from 'react-router-dom'
+import { Container } from './ui/container'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div >
-        <Home />
-        
-      </div>
-    </>
+    <Container>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+    </Container>
   )
 }
 
