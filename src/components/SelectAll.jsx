e3ec5026a070aad2418321f02acd4f2ad6ffb8fa
Jgone2 <jgoneit@gmail.com>
2024-07-10 10:50:08 +0900

    feat: CartPage

---
+++ src/components/SelectAll.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { MdCheckBox, MdCheckBoxOutlineBlank } from "react-icons/md";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  min-width: 400px;
+  max-width: 400px;
+  margin-top: 68px;
+`;
+
+const CheckBox = styled.div`
+  margin-right: 10px; /* 체크박스와 텍스트 사이 간격 */
+  cursor: pointer;
+`;
+
+const Label = styled.span`
+  font-size: 16px;
+`;
+
+function CheckBoxComponent() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <Container>
+      <CheckBox onClick={handleCheckboxChange}>
+        {isChecked ? (
+          <MdCheckBox size={24} />
+        ) : (
+          <MdCheckBoxOutlineBlank size={24} />
+        )}
+      </CheckBox>
+      <Label>전체선택(0/1)</Label>
+      <Label style={{ marginLeft: "auto" }}>선택삭제</Label> {/* 오른쪽 정렬 */}
+    </Container>
+  );
+}
+
+export default CheckBoxComponent;
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PageHeader from "../components/PageHeader";
+import SelectAll from "../components/SelectAll";
+import WishList from "../components/WishList";
+
+const Cart = () => {
+  return (
+    <>
+      <PageHeader />
+      <SelectAll />
+      {/* <WishList /> */}
+    </>
+  );
+};
+
+export default Cart;
