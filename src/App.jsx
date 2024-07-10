dc2bfd540e04a70f84c5f73998c53560ac659eab
Jinseonghyeon <jinsunghyun9@gmail.com>
2024-07-10 14:04:51 +0900

    feat : 판매자 회원가입, 판매자 페이지 화면 완료

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  HashRouter,
+} from "react-router-dom";
+import { Container } from "./ui/container";
+import ItemDetail from "./pages/ItemDetail";
+import Main from "./pages/Main";
+import ItemOrder from "./pages/ItemOrder";
+import Login from "./pages/Login";
+import SignUp from "./pages/Signup";
+import AuthProvider from "./security/AuthContext";
+import Cart from "./pages/Cart";
+import PaymentCompletePage from "./pages/PayCompleted";
+import Category from "./pages/Category";
+import GlobalStyle from "./ui/Font";
+import Search from "./pages/Search";
+import SellerJoin from "./pages/SellerJoin";
+import SellerPage from "./pages/Seller";
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Container>
+          <GlobalStyle />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/itemOrder" element={<ItemOrder />} />
+            <Route path="/details" element={<ItemDetail />} />
+            <Route path="/itemOrder" element={<ItemOrder />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/payCompleted" element={<PaymentCompletePage/>}/>
+            <Route path="/category" element={<Category/>} />
+            <Route path="/search" element={<Search/>} />
+            <Route path="/sellerJoin" element={<SellerJoin/>}/>
+            <Route path="/seller" element={<SellerPage/>}/>
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
