612f750c05e3a26b95a93d7a39d7a8a5a95712dd
twoweekhee <myjjoo4758@icloud.com>
2024-07-11 14:00:46 +0900

    rename

--- src/pages/order/ItemDetail.jsx
+++ src/pages/order/ItemDetail.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 //import SearchHeader from "../../components/SearchHeader";
-import ItemDetailCategory from "../../components/ItemDetailCategory";
+import ItemDetailCategory from "../../components/item/ItemDetailCategory";
 // import ProductCard from "../../components/ProductCard";
 
 const ItemDetail = () => {
